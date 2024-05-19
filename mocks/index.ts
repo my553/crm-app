9c511c72e0d09f2368a359be60567ef3fb3c9764
dmitrijprovotorov <1l1b1v1f1@bk.ru>
2024-05-19 12:39:29 +0300

    feat: add new endpoint to mock server

--- mocks/index.ts
+++ mocks/index.ts
@@ -13,6 +13,8 @@ interface UserDto extends Omit<User, "id"> {}
 
 const usersStorage = new LiveStorage<User[]>("users", [])
 
+const codeStorage = new LiveStorage<string>("code", "")
+
 export const worker = setupWorker(
   http.post("/register", async ({ request }) => {
     const body = (await request.json()) as UserDto
@@ -48,22 +50,70 @@ export const worker = setupWorker(
       return new HttpResponse("Forbidden", { status: 403 })
     }
   }),
-  http.get("/verify-email/:email", async ({ params }) => {
-    const { email } = params as { email: string }
+  http.get("/send-code", async ({ request }) => {
+    const email = new URL(request.url).searchParams.get("email") as string
 
     const candidate = usersStorage
       .getValue()
       .find((candidate) => candidate.email === email)
 
     if (candidate) {
+      const code = faker.string.numeric({ length: 6 })
+
       return HttpResponse.json({
         status: 200,
         data: {
-          code: faker.string.numeric({ length: 6 }),
+          code,
         },
       })
     } else {
       return new HttpResponse("Not found", { status: 404 })
+    }
+  }),
+  http.get("/verify-code", async ({ request }) => {
+    const url = new URL(request.url)
+    const email = url.searchParams.get("email") as string
+    const code = url.searchParams.get("code") as string
+
+    const candidate = usersStorage
+      .getValue()
+      .find((candidate) => candidate.email === email)
+
+    if (candidate && code === codeStorage.getValue()) {
+      return HttpResponse.json({
+        status: 200,
+      })
+    } else {
+      return new HttpResponse("Wrong code", { status: 401 })
+    }
+  }),
+  http.get("/change-password", async ({ request }) => {
+    const { email, newPassword } = (await request.json()) as {
+      newPassword: string
+      email: string
+    }
+
+    const candidate = usersStorage
+      .getValue()
+      .find((candidate) => candidate.email === email)
+
+    if (candidate) {
+      usersStorage.update((prevUsers) =>
+        prevUsers.map((user) =>
+          user.email === email
+            ? {
+                ...user,
+                password: newPassword,
+              }
+            : user,
+        ),
+      )
+
+      return HttpResponse.json({
+        status: 200,
+      })
+    } else {
+      return new HttpResponse("Wrong code", { status: 401 })
     }
   }),
 )
